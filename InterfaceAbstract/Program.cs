d87a7b0b9b8a7efa6e7b6afc5e555ce6c6d67a55
Tim <sniip3r@gmail.com>
2013-05-28 10:42:17 -0400

    first project

---
+++ InterfaceAbstract/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstract
+{
+    class Program
+    {
+        public interface iAnimal
+        {
+            void speak();
+        }
+
+        class Dog : iAnimal
+        {
+            public Dog() { }
+
+            public void speak()
+            {
+                Console.WriteLine("Woof");
+            }
+
+            public void hunt()
+            {
+                Console.WriteLine("Hunt");
+            }
+
+        }
+
+        class Cat : iAnimal
+        {
+            public Cat() { }
+
+            public void speak()
+            {
+                Console.WriteLine("Meow");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            iAnimal dog = new Dog();
+            dog.speak();
+
+            Dog woof = new Dog();
+            woof.hunt();
+
+            Console.ReadLine();
+        }
+    }
+}
